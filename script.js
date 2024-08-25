d8f3b52d4ae44aa2a8ac7a78aa0de2129dad5399
davidgo0098 <david.gomez.vasquez.1998gmail.com>
2024-08-24 20:24:38 -0600

    Carga de archivos

---
+++ script.js
@@ -0,0 +1,7 @@
+// Agregar funcionalidad al botón de registro
+document.addEventListener("DOMContentLoaded", function() {
+    const btnCta = document.querySelector(".btn-cta");
+    btnCta.addEventListener("click", function() {
+        alert("¡Gracias por registrarte!");
+    });
+});
